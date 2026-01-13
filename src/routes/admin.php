b28816647ad9bff8488eab632cbbb6d8b9d87b87
Labrock <vladimirpeshkov5@yandex.ru>
2026-01-13 13:56:57 +0300

    init address

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -6,5 +6,11 @@ Route::middleware(["web", "auth", "app-management"])
     ->prefix("admin")
     ->as("admin.")
     ->group(function () {
-
+        Route::prefix(config("staff-pages.employeePrefix"))
+            ->as("employees.")
+            ->group(function () {
+                Route::get("/", function () {
+                    return "hello staff";
+                })->name("index");
+            });
     });
