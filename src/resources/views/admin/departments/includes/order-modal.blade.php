3403634c57315c99b265e4af1e60ec31cab10f7d
Labrock <vladimirpeshkov5@yandex.ru>
2026-01-15 14:24:32 +0300

    make order modal

--- src/resources/views/admin/departments/includes/order-modal.blade.php
+++ src/resources/views/admin/departments/includes/order-modal.blade.php
@@ -1,1 +1,28 @@
+<x-tt::modal.dialog wire:model="displayOrder">
+    <x-slot name="title">Порядок отделов</x-slot>
+    <x-slot name="content">
+        @if ($list)
+            <x-tt::table drag-root>
+                <x-slot name="head">
+                    <tr>
+                        <x-tt::table.heading class="text-left text-nowrap">Заголовок</x-tt::table.heading>
+                    </tr>
+                </x-slot>
+                <x-slot name="body">
+                    @foreach($list as $key => $item)
+                        <tr drag-item="{{ $item->id }}" drag-item-order="{{ $key }}" wire:key="{{ $item->id }}">
+                            <td>
+                                <div class="flex items-center">
+                                    <x-tt::ico.bars drag-grab class="text-secondary cursor-grab mr-indent" />
+                                    <span class="text-nowrap">{{ $item->title }}</span>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </x-slot>
+            </x-tt::table>
+        @endif
+    </x-slot>
+</x-tt::modal.dialog>
+
 @include("tt::admin.draggable-script")
